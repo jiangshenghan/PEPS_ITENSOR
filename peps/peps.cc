961b4349df90b7994f760f3898ba7bb584ec7faa
shenghan <jiangshenghan@gmail.com>
2015-08-28 10:38:56 -0400

    Finish torus lattice construction. Now construct PEPS structure

--- peps/peps.cc
+++ peps/peps.cc
@@ -1,49 +1,61 @@
 
-#include "peps.h"
+#include "ipeps.h"
 
-PEPS::PEPS(int d, int D, const Lattice_Base& lattice):
+iPEPS(int& d, int& D, const Lattice_uc_Base& lattice_uc):
     d_(d),
     D_(D),
-    lattice_(lattice),
-    site_legs_(lattice.n_sites()),
-    bulk_link_legs_(lattice.n_bulk_links()),
-    boundary_link_legs_(lattice.n_boundary_links()),
-    site_indexsets_(lattice.n_sites_()),
-    site_tensors_(lattice.n_sites_())
+    lattice_uc_(lattice_uc),
+    site_legs_(lattice_uc.n_sites_uc()),
+    link_legs_(lattice_uc.n_links_uc()),
+    site_tensors_(lattice_uc.n_sites_uc()),
+    link_tensors_(lattice_uc.n_links_uc())
 {
-    //Initialize legs
-    for (int i=0; i<lattice_.n_sites_(); i++)
+    //
+    //Initialize site and link legs 
+    //
+    for (int i=0; i<lattice_uc_.n_sites_uc(); i++)
     {
         site_legs_[i]=Index(nameint("Site_leg",i),d_,Site);
     }
-    for (int i=0; i<lattice_.n_bulk_links(); i++)
+
+    for (int i=0; i<lattice_uc_.n_links_uc(); i++)
     {
-        bulk_link_legs_[i]=Index(nameint("Bulk_link_leg",i),D_,Link);
-    }
-    for (int i=0; i<lattice_.n_boundary_links(); i++)
-    {
-        boundary_link_legs_[i]=Index(nameint("Boundary_link_leg",i),D_,Link);
+        link_legs_[i][0]=Index(nameint("Link_leg",2*i),D_,Link);
+        link_legs_[i][1]=Index(nameint("Link_leg",2*i+1),D_,Link);
     }
 
-    //Initialze tensors
-    for (int i=0; i<lattice_.n_sites(); i++)
+    //
+    //Initialize site tensors
+    //
+    for (int i=0; i<lattice_uc_.n_sites_uc(); i++)
     {
-        //Adding site legs and link legs (bulk and boundary to ith siteindex sets)
-        site_indexsets_[i].addIndex(site_legs_[i]);
-        for (int j=0; j<lattice_.n_site_to_links(); j++)
+        IndexSet<Index> site_indexset;
+        site_indexset.addIndex(site_legs_[i]);
+
+        for (int j=0; j<lattice_uc_.n_links_to_one_site(); j++)
         {
-            int add_bulk_leg_j=lattice_.site_to_bulk_links(i,j),
-                add_boundary_leg_j=lattice_.site_to_boundary_links(i,j);
-            if (add_bulk_leg_j!=-1)
-            {
-                site_indexsets_[i].addIndex(bulk_link_legs_[add_bulk_leg_j]);
-            }
-            if (add_boundary_leg_j!=-1)
-            {
-                site_indexsets_[i].addIndex(boundary_link_legs_[add_boundary_legs_j]);
-            }
+            //link_ind stores the sublattice index of jth link which connects to site i
+            Coordinate link_ind=lattice_uc_.site_neighbour_links(i,j);
+            //link_end_ind tells which end of the above link_ind is connected to site i
+            int link_end_ind;
+
+            if (lattice_uc_.link_end_sites(link_ind[2],0)==i)
+                link_end_ind=0;
+            else 
+                link_end_ind=1;
+
+            site_indexset.addIndex(link_legs_[link_ind][link_end_ind]);
         }
 
-        site_tensors_[i]=ITensor(site_indexsets_[i]);
+        site_tensors_[i]=ITensor(site_indexset);
+    }
+
+    //
+    //Initialize link tensors
+    //
+    for (int i=0; i<lattice_uc_.n_links_uc(); i++)
+    {
+        link_tensors_[i]=ITensor(link_tensors_[i][0],link_tensors_[i][1]);
     }
 }
+
--- peps/peps.h
+++ peps/peps.h
@@ -1,40 +1,71 @@
 
-#ifndef _PEPS_H_
-#define _PEPS_H_
+#ifndef _IPEPS_H_
+#define _IPEPS_H_
 
-#include "lattice.h"
+#include "ilattice.h"
 
 //
-//peps class
+//class PEPSt_Torus
+//(the lower "t" stands for "template")
 //
+//Use PEPS_Torus for ITensors,
+//while IQPEPS_Torus for IQTensors
 //
-class PEPS
+class iPEPS
 {
     public:
         //
-        //PEPS Constructors
+        //Constructors
         //
-        PEPS(int d, int D, const Lattice_Base& lattice);
+        iPEPS(int &d, int &D, const Lattice_uc_Base &lattice_uc);
 
         //
-        //PEPS Destructors
+        //Destructors
         //
-        ~PEPS();
+        ~iPEPS();
 
         //
         //Access Methods
         //
+        inline int &d() const
+        {
+            return d_;
+        }
+
+        inline int &D() const
+        {
+            return D_;
+        }
+
+        inline const Lattice_uc_Base &lattice_uc() const
+        {
+            return lattice_uc_;
+        }
+
+        inline ITensor &site_tensors(int i) const
+        {
+            return site_tensors_[i];
+        }
+
+        inline ITensor &link_tensors(int i) const
+        {
+            return link_tensors_[i];
+        }
 
     private:
         //
         //Data Member
         //
+        //d_ is physical index dimension while D_ is virtual leg dim.
         int d_, D_;
-        Lattice_Base& lattice_;
-        vector<Index> site_legs_, bulk_link_legs_, boundary_link_legs_;
-        vector< IndexSet<Index> > site_indexsets_;
-        vector<ITensor> site_tensors_;
+        const Lattice_uc_Base& lattice_uc_;
+        //site_legs_[i] stores indice for physical leg i, where i labels the sublattice. One should be able to generate legs (x,y,i) by site_legs_[i].
+        vector<Index> site_legs_;
+        //link_legs_[i] stores indice for two indices associated with link i, where i is the sublattice.
+        vector< array<Index,2> > link_legs_;
+        //site_tensors_[i] is an abstract tensor for site with sublattice i. Similar for link_tensors[i]
+        //we set link tensor as diagonal matrix
+        vector<ITensor> site_tensors_, link_tensors_;
 };
-
 
 #endif
