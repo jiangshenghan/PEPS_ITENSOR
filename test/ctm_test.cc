8141fc3f0ae4601c49cfd33dc4e04408482d4132
shenghan <jiangshenghan@gmail.com>
2015-12-16 16:00:10 -0500

    12/16/2015__debug_ctm

---
+++ test/ctm_test.cc
@@ -0,0 +1,32 @@
+
+#include "tnetwork_storage.h"
+#include "corner_transfer_mat.h"
+
+int main()
+{
+    //reading tnetwork from file
+    std::stringstream ss;
+    ss << "/home/jiangsb/code/peps_itensor/result/peps_storage/square_srvb_Lx=4_Ly=4.txt";
+    std::string file_name=ss.str();
+    Tnetwork_Storage<IQTensor> square_srvb_tnetwork;
+    readFromFile(filename,square_srvb_tnetwork);
+    //check tnetwork from file
+    Print(square_srvb_tnetwork._Lx);
+    Print(square_srvb_tnetwork._Ly);
+    PrintDat(square_srvb_tnetwork._tensor_list);
+
+    //stores ordered virtual indices 
+    const auto &tensor_list=square_srvb_tnetwork._tensor_list;
+    int Lx=square_srvb_tnetwork._Lx,
+        Ly=square_srvb_tnetwork._Ly;
+    std::array<IQIndex,4> tens0_ordered_virt_indices;
+    tens0_ordered_virt_indices[0]=commonIndex(tensor_list(0),tensor_list(Lx-1));
+    tens0_ordered_virt_indices[1]=commonIndex(tensor_list(0),tensor_list(Lx));
+    tens0_ordered_virt_indices[1]=commonIndex(tensor_list(0),tensor_list(1));
+    tens0_ordered_virt_indices[1]=commonIndex(tensor_list(0),tensor_list(Lx*(Ly-1)));
+
+    Corner_Transfer_Matrix square_srvb_ctm=({tensor_list(0)},{tens0_ordered_virt_indices});
+    
+
+    return 0;
+}
